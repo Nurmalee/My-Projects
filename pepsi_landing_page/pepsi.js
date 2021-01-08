dfa9183ea6ff7382e94e7f6274737678f24b66c4
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-01-08 21:54:53 +0100

    new updates

--- pepsi_landing_page/pepsi.js
+++ pepsi_landing_page/pepsi.js
@@ -3,7 +3,6 @@ const productImage = document.querySelectorAll(".product-control .product-image"
 Array.from(productImage).forEach(function(productImageItem){
     productImageItem.addEventListener("mouseenter", function(){
         const image = productImageItem.querySelector("img");
-        // console.log(image.getAttribute("src"));
         productHovered = image.getAttribute("src");
         const productImage = document.querySelector(".product .product-image img");
         productImage.src = productHovered;
